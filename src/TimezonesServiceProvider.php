40d4d68bd8c424ea8e9ad438d20a70347ad513a0
myciplnew <myciplnew@gmail.com>
2018-07-11 17:29:08 +0530

    Service provider update

--- src/TimezonesServiceProvider.php
+++ src/TimezonesServiceProvider.php
@@ -16,7 +16,7 @@ class TimezonesServiceProvider extends ServiceProvider
         include __DIR__.'/routes/web.php';
         $this->loadViewsFrom(__DIR__.'/views', 'timezones');
         $this->publishes([
-            __DIR__.'/views' => base_path('resources/views/laraveldaily/timezones'),
+            __DIR__.'/views' => base_path('resources/views/myciplnew/timezones'),
         ]);
     }
 
